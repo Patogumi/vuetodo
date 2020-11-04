8a15c6036167413f936ca5cd3eac5e97bb7c0dae
Patogumi <64481608+Patogumi@users.noreply.github.com>
2020-11-04 18:49:15 +0900

    Primer commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Componente Vue</div>
+
+                    <div class="card-body">
+                        Hola, soy un componente de Vue, modificado por don Patricio, mi Dueño y Señor. A quien sirvo y amo. 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
